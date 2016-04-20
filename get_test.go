61221d2b24b7dbc81a6a36d638fb96c626182012
Robert McNeil (porty) <pppppppman@gmail.com>
2016-04-21 00:14:15 +1000

    Easier tests

--- get_test.go
+++ get_test.go
@@ -9,6 +9,12 @@ import (
 	"testing"
 )
 
+func expectEqualsStr(t *testing.T, actual string, expected string, message string) {
+	if expected != actual {
+		t.Errorf("%s. Expected '%s', actual '%s'", message, expected, actual)
+	}
+}
+
 func getProxy() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(Get))
 }
@@ -25,6 +31,10 @@ func getClient(proxy *url.URL) *http.Client {
 
 func TestGet(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expectEqualsStr(t, r.Method, http.MethodGet, "Bad method")
+		expectEqualsStr(t, r.Proto, "HTTP/1.1", "Bad proto")
+		expectEqualsStr(t, r.RequestURI, "/rofl", "Bad request URI")
+
 		fmt.Fprint(w, "Hello, client")
 	}))
 	defer ts.Close()
@@ -51,12 +61,6 @@ func TestGet(t *testing.T) {
 	expected := "Hello, client"
 	actual := string(body)
 
-	if actual != expected {
-		t.Errorf("Content mismatch, expected '%s', received '%s'", expected, actual)
-	}
-
-	if resp.Header.Get("X-rob-proxy") != "0.1" {
-		t.Errorf("Expected X-Rob-Proxy header value of 0.1, receieved %s", resp.Header.Get("X-rob-proxy"))
-	}
-
+	expectEqualsStr(t, actual, expected, "Bad content")
+	expectEqualsStr(t, resp.Header.Get("X-rob-proxy"), "0.1", "Bad X-Rob-Proxy header")
 }
